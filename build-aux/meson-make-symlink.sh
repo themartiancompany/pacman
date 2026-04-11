8825d3183dfd5f15a7aa1f85fbb19b25a135ab92
Pellegrino Prevete <dvorak@0x87003Bd6C074C713783df04f36517451fF34CBEf>
2026-04-11 22:44:03 +0000

    hurr

--- build-aux/meson-make-symlink.sh
+++ build-aux/meson-make-symlink.sh
@@ -4,7 +4,41 @@ set -eu
 # this is needed mostly because $DESTDIR is provided as a variable,
 # and we need to create the target directory...
 
-mkdir -vp "$(dirname "${DESTDIR:-}$2")"
+_os="$(
+  uname \
+    -o)"
 
-rm -f "${DESTDIR:-}$2"
-ln -vs "$1" "${DESTDIR:-}$2"
+_global_variables() {
+  _target_file=""
+  _target_link=""
+  _internal_path=""
+  _destdir=""
+}
+
+_global_variables
+
+_target_file="${1}"
+_internal_path="${2}"
+if [[ -v "DESTDIR" ]]; then
+  _destdir="${DESTDIR}"
+elif [[ ! -v "DESTDIR" ]]; then
+  _destdir=""
+  if (( 2 < "$#" )); then
+    _destdir="${3}"
+  fi
+fi
+if [[ "${_os}" == "Android" ]]; then
+  _destdir="@ANDROID_ROOT@"
+fi
+_target_link="${_destdir}${_internal_path}"
+mkdir \
+  -vp \
+  "$(dirname \
+       "${_target_link}")"
+rm \
+  -f \
+  "${_target_link}"
+ln \
+  -vs \
+  "${_target_file}" \
+  "${_target_link}"
